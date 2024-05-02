dc529836feb5800e16a2b0f4e4bddc022182ebdd
Vamshix57 <vamshi.krishna@kalvium.community>
2024-05-02 14:52:48 +0530

    Done

--- hooks/src/main.jsx
+++ hooks/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ParentContext from './components/UseContext/ParentContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
+  <ParentContext>
     <App />
-  </React.StrictMode>,
+    </ParentContext>
+  </>,
 )
